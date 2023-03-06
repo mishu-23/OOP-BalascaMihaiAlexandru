a71d713107819d42d98af2d857f6881877647668
mishu-23 <85368107+mishu-23@users.noreply.github.com>
2023-03-06 15:38:08 +0200

    Add files via upload

---
+++ oop/lab3/ex2/ex2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Canvas.h"
+using namespace std;
+
+int main() {
+	Canvas c(11,11);
+	c.DrawCircle(5, 5, 5, 'x');
+	c.FillCircle(5, 5, 5, '@');
+	c.Print();
+	cout << endl;
+	c.Clear();
+	c.DrawRect(1, 1, 9, 9, '+');
+	c.FillRect(1, 1, 9, 9, '/');
+	c.Print();
+	return 0;
+}
